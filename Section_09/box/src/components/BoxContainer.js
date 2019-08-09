cd82df9aeda7855bcd43eac7401ff258c819ad79
William M <wmooney1984@gmail.com>
2019-08-09 18:58:38 -0400

    Section 09: Box App - Completed

---
+++ Section_09/box/src/components/BoxContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Box } from './Box';
+import './BoxContainer.css';
+
+export class BoxContainer extends Component {
+  static defaultProps = {
+    numBoxes: 25
+  }
+
+  render() {
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+      <Box />
+    ));
+
+    return <div className="BoxContainer">{boxes}</div>;
+  }
+}
